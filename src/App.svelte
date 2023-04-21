2c432f0ae34e1edf084e0da7d0b1ec25bfbd6413
mlaadd <61882328+mlaadd@users.noreply.github.com>
2023-04-21 23:58:27 +0200

    simple vorwärtskalkulation hinzugefügt

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,60 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+  import { kalkulationsEintrag } from "./lib/kalkulationsEintrag";
+	
+	const kalkulation = Object.entries({
+		listeneinkaufspreis: new kalkulationsEintrag("Listeneinkaufspreis", 100, undefined, true, false),
+		lieferantenrabatt: new kalkulationsEintrag("Lieferantenrabatt", undefined, 10, true, true, true),
+		zieleinkaufspreis: new kalkulationsEintrag("Zieleinkaufspreis", undefined, undefined, false, false),
+		lieferantenskonto: new kalkulationsEintrag("Lieferantenskonto", undefined, 2, true, true, true),
+		bareinkaufspreis: new kalkulationsEintrag("Bareinkaufspreis", undefined, undefined, false, false),
+		bezugskosten: new kalkulationsEintrag("Bareinkaufspreis", 11.8, undefined, true, true),
+		einstandspreis: new kalkulationsEintrag("Einstandspreis (Bezugspreis)", undefined, undefined, false, false),
+		gemeinkosten: new kalkulationsEintrag("Gemeinkosten", undefined, 40, true, true),
+		selbstkosten: new kalkulationsEintrag("Selbstkosten", undefined, undefined, false, false),
+		gewinnzuschlag: new kalkulationsEintrag("Gewinnzuschlag", undefined, 25, true, true),
+		barverkaufspreis: new kalkulationsEintrag("Barverkaufspreis", undefined, undefined, false, false),
+		kundenskonto: new kalkulationsEintrag("Kundenskonto", undefined, 3, true, true, false, false),
+		zielverkaufspreis: new kalkulationsEintrag("Zielverkaufspreis", undefined, undefined, false, false),
+		kundenrabatt: new kalkulationsEintrag("Kundenrabatt", undefined, 10, true, true, false, false),
+		listenverkaufspreisNetto: new kalkulationsEintrag("Listenverkaufspreis (netto)", undefined, undefined, false, false),
+		umsatzsteuer: new kalkulationsEintrag("Umsatzsteuer", undefined, 19, true, true),
+		listenverkaufspreisBrutto: new kalkulationsEintrag("Listenverkaufspreis (brutto)", undefined, undefined)
+	});
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<table>
+	<tr><th>Kategorie</th><th>%</th><th>€</th></tr>
+	{#each kalkulation as [name, wert], index}
+		<tr>
+			<td>{wert.name}</td>
+			{#if wert.modifizierend}
+				<td>
+					{#if wert.istBearbeitbar}
+						{#if wert.prozent}
+							<input type="number" bind:value={wert.prozent}>
+						{:else}
+							<input type="number" bind:value={wert.wert}>
+						{/if}
+					{:else}
+						{wert.wert}
+					{/if}
+				</td>
+				{#if wert.ist100}
+					<td>{(wert.wert = +(wert.prozent ? kalkulation[index-1][1].wert * wert.prozent / 100 : wert.wert).toFixed(2))}</td>
+				{:else}
+					<td>{(wert.wert = +(wert.prozent ? kalkulation[index-1][1].wert / (100-wert.prozent) * wert.prozent : wert.wert)).toFixed(2)}</td>
+				{/if}
+			{:else}
+				<td></td>
+				<td>
+					{#if wert.istBearbeitbar}
+						<input type="number" bind:value={wert.wert}>
+					{:else}
+						{(wert.wert = +(kalkulation[index-2][1].wert + kalkulation[index-1][1].abzug)).toFixed(2)}
+					{/if}
+				</td>
+			{/if}
+		</tr>
+	{/each}
+</table>
